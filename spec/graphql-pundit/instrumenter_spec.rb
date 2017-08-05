8ce78ebed5f8685f2e82292503e032d81b8812fb
Tom Gehrke <phyrog@cognitive-coding.com>
2017-08-05 11:57:43 +0200

    Add option to pass lambda to authorize

--- spec/graphql-pundit/instrumenter_spec.rb
+++ spec/graphql-pundit/instrumenter_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "spec_helper"
 
 class Test
@@ -16,7 +17,7 @@ class TestPolicy
   end
 
   def create?
-    @value.to_s == "pass"
+    @value.to_s == 'pass'
   end
 end
 
@@ -25,52 +26,102 @@ RSpec.describe GraphQL::Pundit::Instrumenter do
   let(:instrumented_field) { instrumenter.instrument(nil, field) }
   let(:fail) { Test.new(:fail) }
   let(:pass) { Test.new(:pass) }
-  let(:result) { instrumented_field.resolve(subject, {}, {})}
+  let(:result) { instrumented_field.resolve(subject, {}, {}) }
 
   context 'authorize' do
-    let(:field) do
-      GraphQL::Field.define(type: "String") do
-        name "TestField"
-        authorize :create
-        resolve ->(obj, args, ctx) { obj.to_s }
+    context 'with symbol' do
+      let(:field) do
+        GraphQL::Field.define(type: 'String') do
+          name 'TestField'
+          authorize :create
+          resolve ->(obj, _args, _ctx) { obj.to_s }
+        end
+      end
+
+      context 'Authorized' do
+        subject { pass }
+        it 'returns the value' do
+          expect(result).to eq('pass')
+        end
+      end
+
+      context 'Unauthorized' do
+        subject { fail }
+        it 'returns nil' do
+          expect(result).to eq(nil)
+        end
       end
     end
 
-    context 'Authorized' do
-      subject { pass }
-      it 'returns the value' do
-        expect(result).to eq("pass")
+    context 'authorize!' do
+      let(:field) do
+        GraphQL::Field.define(type: 'String') do
+          name 'TestField'
+          authorize! :create
+          resolve ->(obj, _args, _ctx) { obj.to_s }
+        end
+      end
+
+      context 'Authorized' do
+        subject { pass }
+        it 'returns the value' do
+          expect(result).to eq('pass')
+        end
+      end
+
+      context 'Unauthorized' do
+        subject { fail }
+        it 'raises an execution error' do
+          expect { result }.to raise_error(GraphQL::ExecutionError)
+        end
       end
     end
 
-    context 'Unauthorized' do
-      subject { fail }
-      it 'returns nil' do
-        expect(result).to eq(nil)
+    context 'with proc' do
+      let(:field) do
+        GraphQL::Field.define(type: 'String') do
+          name 'TestField'
+          authorize ->(obj, _args, _ctx) { TestPolicy.new(nil, obj).create? }
+          resolve ->(obj, _args, _ctx) { obj.to_s }
+        end
+      end
+
+      context 'Authorized' do
+        subject { pass }
+        it 'returns the value' do
+          expect(result).to eq('pass')
+        end
+      end
+
+      context 'Unauthorized' do
+        subject { fail }
+        it 'returns nil' do
+          expect(result).to eq(nil)
+        end
       end
     end
-  end
 
-  context 'authorize!' do
-    let(:field) do
-      GraphQL::Field.define(type: "String") do
-        name "TestField"
-        authorize! :create
-        resolve ->(obj, args, ctx) { obj.to_s }
+    context 'authorize!' do
+      let(:field) do
+        GraphQL::Field.define(type: 'String') do
+          name 'TestField'
+          authorize! ->(obj, _args, _ctx) { TestPolicy.new(nil, obj).create? }
+          resolve ->(obj, _args, _ctx) { obj.to_s }
+        end
       end
-    end
 
-    context 'Authorized' do
-      subject { pass }
-      it 'returns the value' do
-        expect(result).to eq("pass")
+      context 'Authorized' do
+        subject { pass }
+        it 'returns the value' do
+          expect(result).to eq('pass')
+        end
       end
-    end
 
-    context 'Unauthorized' do
-      subject { fail }
-      it 'raises an execution error' do
-        expect { result }.to raise_error(GraphQL::ExecutionError)
+      context 'Unauthorized' do
+        subject { fail }
+        it 'raises an execution error' do
+          expect { result }.to raise_error(GraphQL::ExecutionError)
+        end
       end
     end
   end
