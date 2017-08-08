35f3db35fd87ba3383357f66e5cf44010a52d312
Tom Gehrke <phyrog@cognitive-coding.com>
2017-08-08 19:05:44 +0200

    Obey hound

--- spec/graphql-pundit/instrumenter_spec.rb
+++ spec/graphql-pundit/instrumenter_spec.rb
@@ -25,8 +25,8 @@ end
 RSpec.describe GraphQL::Pundit::Instrumenter do
   let(:instrumenter) { GraphQL::Pundit::Instrumenter.new }
   let(:instrumented_field) { instrumenter.instrument(nil, field) }
-  let(:fail) { Test.new(:fail) }
-  let(:pass) { Test.new(:pass) }
+  let(:fail_test) { Test.new(:fail) }
+  let(:pass_test) { Test.new(:pass) }
   let(:result) { instrumented_field.resolve(subject, {}, {}) }
 
   context 'authorize' do
@@ -40,14 +40,14 @@ RSpec.describe GraphQL::Pundit::Instrumenter do
       end
 
       context 'Authorized' do
-        subject { pass }
+        subject { pass_test }
         it 'returns the value' do
           expect(result).to eq('pass')
         end
       end
 
       context 'Unauthorized' do
-        subject { fail }
+        subject { fail_test }
         it 'returns nil' do
           expect(result).to eq(nil)
         end
@@ -64,14 +64,14 @@ RSpec.describe GraphQL::Pundit::Instrumenter do
       end
 
       context 'Authorized' do
-        subject { pass }
+        subject { pass_test }
         it 'returns the value' do
           expect(result).to eq('pass')
         end
       end
 
       context 'Unauthorized' do
-        subject { fail }
+        subject { fail_test }
         it 'raises an execution error' do
           expect { result }.to raise_error(GraphQL::ExecutionError)
         end
@@ -88,14 +88,14 @@ RSpec.describe GraphQL::Pundit::Instrumenter do
       end
 
       context 'Authorized' do
-        subject { pass }
+        subject { pass_test }
         it 'returns the value' do
           expect(result).to eq('pass')
         end
       end
 
       context 'Unauthorized' do
-        subject { fail }
+        subject { fail_test }
         it 'returns nil' do
           expect(result).to eq(nil)
         end
@@ -112,14 +112,14 @@ RSpec.describe GraphQL::Pundit::Instrumenter do
       end
 
       context 'Authorized' do
-        subject { pass }
+        subject { pass_test }
         it 'returns the value' do
           expect(result).to eq('pass')
         end
       end
 
       context 'Unauthorized' do
-        subject { fail }
+        subject { fail_test }
         it 'raises an execution error' do
           expect { result }.to raise_error(GraphQL::ExecutionError)
         end
