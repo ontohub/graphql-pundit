1317289d4e2d3bdaa03fabc0f78c9d2ceee464e9
Tom Gehrke <phyrog@cognitive-coding.com>
2017-08-08 12:59:04 +0200

    Allow zero arguments for authorize

--- spec/graphql-pundit/instrumenter_spec.rb
+++ spec/graphql-pundit/instrumenter_spec.rb
@@ -17,8 +17,82 @@ class TestPolicy
     @value = value
   end
 
-  def create?
+  def test?
     @value.to_s == 'pass'
+  end
+end
+
+RSpec.shared_examples 'an authorizing field' do |error|
+  context 'Authorized' do
+    subject { pass_test }
+    it 'returns the value' do
+      expect(result).to eq('pass')
+    end
+  end
+
+  context 'Unauthorized' do
+    subject { fail_test }
+    if error
+      it 'raises an execution error' do
+        expect { result }.to raise_error(GraphQL::ExecutionError)
+      end
+    else
+      it 'returns nil' do
+        expect(result).to eq(nil)
+      end
+    end
+  end
+end
+
+RSpec.shared_examples 'field with authorization' do |error|
+  context 'with query' do
+    context 'with name' do
+      let(:field) do
+        GraphQL::Field.define(type: 'String') do
+          name :test
+          if error
+            authorize! :test
+          else
+            authorize :test
+          end
+          resolve ->(obj, _args, _ctx) { obj.to_s }
+        end
+      end
+
+      include_examples 'an authorizing field', error
+    end
+
+    context 'with proc' do
+      let(:field) do
+        GraphQL::Field.define(type: 'String') do
+          name :notTest
+          if error
+            authorize! ->(obj, _args, _ctx) { TestPolicy.new(nil, obj).test? }
+          else
+            authorize ->(obj, _args, _ctx) { TestPolicy.new(nil, obj).test? }
+          end
+          resolve ->(obj, _args, _ctx) { obj.to_s }
+        end
+      end
+
+      include_examples 'an authorizing field', error
+    end
+  end
+
+  context 'without query' do
+    let(:field) do
+      GraphQL::Field.define(type: 'String') do
+        name :test
+        if error
+          authorize!
+        else
+          authorize
+        end
+        resolve ->(obj, _args, _ctx) { obj.to_s }
+      end
+    end
+
+    include_examples 'an authorizing field', error
   end
 end
 
@@ -30,100 +104,10 @@ RSpec.describe GraphQL::Pundit::Instrumenter do
   let(:result) { instrumented_field.resolve(subject, {}, {}) }
 
   context 'authorize' do
-    context 'with symbol' do
-      let(:field) do
-        GraphQL::Field.define(type: 'String') do
-          name 'TestField'
-          authorize :create
-          resolve ->(obj, _args, _ctx) { obj.to_s }
-        end
-      end
+    include_examples 'field with authorization', false
+  end
 
-      context 'Authorized' do
-        subject { pass_test }
-        it 'returns the value' do
-          expect(result).to eq('pass')
-        end
-      end
-
-      context 'Unauthorized' do
-        subject { fail_test }
-        it 'returns nil' do
-          expect(result).to eq(nil)
-        end
-      end
-    end
-
-    context 'authorize!' do
-      let(:field) do
-        GraphQL::Field.define(type: 'String') do
-          name 'TestField'
-          authorize! :create
-          resolve ->(obj, _args, _ctx) { obj.to_s }
-        end
-      end
-
-      context 'Authorized' do
-        subject { pass_test }
-        it 'returns the value' do
-          expect(result).to eq('pass')
-        end
-      end
-
-      context 'Unauthorized' do
-        subject { fail_test }
-        it 'raises an execution error' do
-          expect { result }.to raise_error(GraphQL::ExecutionError)
-        end
-      end
-    end
-
-    context 'with proc' do
-      let(:field) do
-        GraphQL::Field.define(type: 'String') do
-          name 'TestField'
-          authorize ->(obj, _args, _ctx) { TestPolicy.new(nil, obj).create? }
-          resolve ->(obj, _args, _ctx) { obj.to_s }
-        end
-      end
-
-      context 'Authorized' do
-        subject { pass_test }
-        it 'returns the value' do
-          expect(result).to eq('pass')
-        end
-      end
-
-      context 'Unauthorized' do
-        subject { fail_test }
-        it 'returns nil' do
-          expect(result).to eq(nil)
-        end
-      end
-    end
-
-    context 'authorize!' do
-      let(:field) do
-        GraphQL::Field.define(type: 'String') do
-          name 'TestField'
-          authorize! ->(obj, _args, _ctx) { TestPolicy.new(nil, obj).create? }
-          resolve ->(obj, _args, _ctx) { obj.to_s }
-        end
-      end
-
-      context 'Authorized' do
-        subject { pass_test }
-        it 'returns the value' do
-          expect(result).to eq('pass')
-        end
-      end
-
-      context 'Unauthorized' do
-        subject { fail_test }
-        it 'raises an execution error' do
-          expect { result }.to raise_error(GraphQL::ExecutionError)
-        end
-      end
-    end
+  context 'authorize!' do
+    include_examples 'field with authorization', true
   end
 end
