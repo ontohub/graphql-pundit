b71489af60bbee267a42ccca70ef143abed18b8a
Tom Gehrke <phyrog@cognitive-coding.com>
2017-10-27 09:52:48 +0200

    Refactor scope instrumenter

--- lib/graphql-pundit/instrumenters/scope.rb
+++ lib/graphql-pundit/instrumenters/scope.rb
@@ -7,52 +7,69 @@ module GraphQL
     module Instrumenters
       # Instrumenter that supplies `scope`
       class Scope
+        # Applies the scoping to the passed object
+        class ScopeResolver
+          attr_reader :current_user, :scope, :old_resolver
+
+          def initialize(current_user, scope, old_resolver)
+            @current_user = current_user
+            @old_resolver = old_resolver
+
+            unless valid_value?(scope)
+              raise ArgumentError, 'Invalid value passed to `scope`'
+            end
+
+            @scope = new_scope(scope)
+          end
+
+          def call(root, arguments, context)
+            new_scope = scope.call(root, arguments, context)
+            old_resolver.call(new_scope, arguments, context)
+          end
+
+          private
+
+          def new_scope(scope)
+            return scope if proc?(scope)
+
+            lambda do |root, _arguments, context|
+              unless inferred?(scope)
+                root.define_singleton_method(:policy_class) { scope }
+              end
+
+              ::Pundit.policy_scope!(context[current_user], root)
+            end
+          end
+
+          def valid_value?(value)
+            value.is_a?(Class) || inferred?(value) || proc?(value)
+          end
+
+          def proc?(value)
+            value.respond_to?(:call)
+          end
+
+          def inferred?(value)
+            value == :infer_scope
+          end
+        end
+
         attr_reader :current_user
 
         def initialize(current_user = :current_user)
           @current_user = current_user
         end
 
-        # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
         def instrument(_type, field)
-          # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
           scope = field.metadata[:scope]
           return field unless scope
-          unless valid_value?(scope)
-            raise ArgumentError, 'Invalid value passed to `scope`'
-          end
 
-          old_resolve = field.resolve_proc
-
-          scope_proc = lambda do |obj, _args, ctx|
-            unless inferred?(scope)
-              obj.define_singleton_method(:policy_class) { scope }
-            end
-
-            ::Pundit.policy_scope!(ctx[current_user], obj)
-          end
-          scope_proc = scope if proc?(scope)
+          old_resolver = field.resolve_proc
+          resolver = ScopeResolver.new(current_user, scope, old_resolver)
 
           field.redefine do
-            resolve(lambda do |obj, args, ctx|
-              new_scope = scope_proc.call(obj, args, ctx)
-              old_resolve.call(new_scope, args, ctx)
-            end)
+            resolve resolver
           end
-        end
-
-        private
-
-        def valid_value?(value)
-          value.is_a?(Class) || inferred?(value) || proc?(value)
-        end
-
-        def proc?(value)
-          value.respond_to?(:call)
-        end
-
-        def inferred?(value)
-          value == :infer_scope
         end
       end
     end
