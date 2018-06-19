e42478930848b56441ec1b35bfcb5fe4afa23fda
Tom Gehrke <phyrog@cognitive-coding.com>
2018-06-19 18:23:02 +0200

    Only define Field if using graphql >= 1.8 to avoid a load error

--- lib/graphql-pundit/field.rb
+++ lib/graphql-pundit/field.rb
@@ -6,10 +6,13 @@ require 'graphql-pundit/scope'
 
 module GraphQL
   module Pundit
-    # Field class that contains authorization and scope behavior
-    class Field < GraphQL::Schema::Field
-      prepend GraphQL::Pundit::Scope
-      prepend GraphQL::Pundit::Authorization
+    if defined?(GraphQL::Schema::Field)
+      # Field class that contains authorization and scope behavior
+      # This only works with graphql >= 1.8.0
+      class Field < GraphQL::Schema::Field
+        prepend GraphQL::Pundit::Scope
+        prepend GraphQL::Pundit::Authorization
+      end
     end
   end
 end
--- lib/graphql-pundit/version.rb
+++ lib/graphql-pundit/version.rb
@@ -2,6 +2,6 @@
 
 module GraphQL
   module Pundit
-    VERSION = '0.7.0'
+    VERSION = '0.7.1'
   end
 end
